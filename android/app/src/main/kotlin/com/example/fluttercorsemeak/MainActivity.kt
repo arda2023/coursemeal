7dda3f81cd70288b7d3bd9fb2445c3acdcd53f27
Arda <ardasa716@gmail.com>
2026-06-10 10:51:47 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/fluttercorsemeak/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fluttercorsemeak
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screens/tabs.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:fluttercorsemeak/screens/categories.dart';
+import 'package:fluttercorsemeak/screens/meals.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({super.key});
+
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget activePage = CategoriesScreen();
+    var activePageTitle = "Categories";
+
+    if (_selectedPageIndex == 1) {
+      activePage = MealsScreen(title: "Favorites", meals: []);
+      activePageTitle = "Your favortes";
+    }
+    return Scaffold(
+      appBar: AppBar(title: Text(activePageTitle)),
+      body: activePage,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        currentIndex: _selectedPageIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.set_meal),
+            label: "Categories",
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: "Favorites"),
+        ],
+      ),
+    );
+  }
+}
